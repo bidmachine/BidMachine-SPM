3033e4f24d146bec2dd8832c851cf6b36be97ec8
Dzmitry Paplauski <dzmitry@MacBook-Pro-Dzmitry.local>
2026-01-23 12:21:08 +0100

    update OMSDK hash

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         .binaryTarget(
             name: "OMSDK_Appodeal",
             url: "https://s3-us-west-1.amazonaws.com/appodeal-ios/external-sdks/OMSDK_Appodeal/1.6.0/SPM/OMSDK_Appodeal.zip",
-            checksum: "2dc9382ce748e1e173175915bbfe1e17d162e26d248c283448d14dea62369880"
+            checksum: "0e952e40640f61bb2db2d4ef1a2bd741cbe7bd95534eb75fed2f70a70d04f800"
         ),
         .target(
             name: "BidMachineTarget",
