e75e2dd79587c873927c63052ea1eea7a9e16ce6
Ilia Lozhkin <ilia.log.1990@gmail.com>
2026-08-26 12:24:40 +0300

    Update to 3.7.2

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BidMachine",
-            url: "https://bidmachine-ios.s3.amazonaws.com/BidMachine/3.7.1/package/BidMachine.xcframework.zip",
-            checksum: "a033bfaccb55467a1dda8012110534726dd59337f9203db47cfafe023d5b1271"
+            url: "https://bidmachine-ios.s3.amazonaws.com/BidMachine/3.7.2-beta.0/package/BidMachine.xcframework.zip",
+            checksum: "39787f0e9d494446b20122fab44f580133fbaabd32c0ca37e97413e3a8e6accc"
         ),
         .target(
             name: "BidMachineTarget",
